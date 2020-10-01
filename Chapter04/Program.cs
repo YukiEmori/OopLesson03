a88bd143bc36770d4646175bd04d4a0114433f7b
YukiEmori <31040@ojs.ac.jp>
2020-10-01 10:20:56 +0900

    P116リスト4-27

--- Chapter04/Program.cs
+++ Chapter04/Program.cs
@@ -7,6 +7,26 @@ using System.Threading.Tasks;
 namespace Chapter04 {
     class Program {
         static void Main(string[] args) {
+            #region null合体演算子
+            string code = "12345";
+
+            var message = GetMessage(code) ?? DefaultMessage();
+            Console.WriteLine(message);
+        }
+
+        //スタブ
+        private static object DefaultMessage() {
+            return "DefaultMessage";
+        }
+
+        //スタブ
+        private static object GetMessage(string code) {
+            return code;
+            #endregion
+
+            #region null条件演算子
+
+            #endregion
         }
     }
 }
