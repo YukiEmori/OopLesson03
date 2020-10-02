fa0e92e066d9047f64606f964967a4f2173fe8e0
YukiEmori <31040@ojs.ac.jp>
2020-10-02 15:03:09 +0900

    第4章の遠陬問題P133～P134

--- Chapter04/Program.cs
+++ Chapter04/Program.cs
@@ -8,44 +8,52 @@ using System.Threading.Tasks;
 namespace Chapter04 {
     class Program {
         static void Main(string[] args) {
-            #region null合体演算子
-            //    string code = "12345";
-
-            //    var message = GetMessage(code) ?? DefaultMessage();
-            //    Console.WriteLine(message);
-            //}
-
-            ////スタブ
-            //private static object DefaultMessage() {
-            //    return "DefaultMessage";
-            //}
-
-            ////スタブ
-            //private static object GetMessage(string code) {
-            //    return code;
-            #endregion
-
-            #region null条件演算子
-            Console.WriteLine(GetProdcut());
-            #endregion
-
-        }
-        private static string  GetProdcut() {
-            Sale sale = new Sale() {
-                ShopName = "pet store",
-                Amount = 10000,
-                Product = "food",
+            //4-2-1
+            var YearMonthArray = new YearMonth[] {
+                new YearMonth(2000,1),
+                new YearMonth(2001,12),
+                new YearMonth(2020,11),
+                new YearMonth(2100,3),
+                new YearMonth(2101,12),
             };
-            sale = null;
-            return sale?.Product;
-        }
-    }
 
-    class Sale {
-        //店舗名
-        public string ShopName { get; set; }
-        //売上高
-        public int Amount { get; set; }
-        public string Product { get; set; }
+            //4-2-2
+            Console.WriteLine("-----4-2-2-----");
+            foreach (var array in YearMonthArray) {
+                Console.WriteLine(array);
+            }
+            Console.WriteLine();
+
+            //4-2-4
+            Console.WriteLine("-----4-2-4-----");
+            var Is21 = FindFirst21C(YearMonthArray);
+            if (Is21 == null) 
+                Console.WriteLine("21世紀のデータはありません");
+            else
+                Console.WriteLine(Is21);
+            Console.WriteLine();
+
+            //4-2-5
+            Console.WriteLine("-----4-2-5-----");
+            var arrays = YearMonthArray.Select(ym => ym.AddOneMonth()).ToArray();
+            foreach (var ym in arrays) {
+                Console.WriteLine(ym);
+            }
+
+        }
+
+        //4-2-3
+        static YearMonth FindFirst21C(YearMonth[] yms) {
+            foreach (var array in yms) {
+                if (array.Is21Century) {
+                    return array;
+                }
+            }
+            return null;
+        }
+
+
     }
+    
 }
+
---
+++ Chapter04/YearMonth.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter04 {
+
+    class YearMonth {
+        //4.1.1
+        //プロパティ
+        public int Year { get; private set; }
+        public int Month { get; private set; }
+
+        //コンストラクタ
+        public YearMonth(int Year, int Month) {
+            this.Year = Year;
+            this.Month = Month;
+        }
+
+        //4.1.2
+        public bool Is21Century
+        {
+            get {
+                return 2001 <= Year && Year <= 2100;  //p109参照 
+            }
+        }
+                
+
+
+        //4.1.3
+        public YearMonth AddOneMonth() {
+            if(Month == 12) {
+                return new YearMonth(Year + 1 , 1);
+            } else {
+                return new YearMonth(Year, Month + 1);
+            }
+            
+        }
+
+        //4.1.4
+        public override string ToString() {
+            return $"{Year}年{Month}月";
+        }
+
+
+    }
+    
+}
