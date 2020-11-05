6488ea4a6c0b01a06020d695e4b1f4b249e9c0ee
YukiEmori <31040@ojs.ac.jp>
2020-11-05 11:19:35 +0900

    設定クラス作成

---
+++ SendMailApp/Config.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace SendMailApp {
+    class Config {
+        public string Smtp { get; set; }    //SMTPサーバ
+        public string MailAddres { get; set; }  //自メールアドレス(送信元)
+        public string PassWord { get; set; }    //パスワード
+        public int Port { get; set; }       //ポート番号
+        public bool Ssl { get; set; }       //SSL設定
+
+        //初期値
+        public void DefaultSet() {
+            Smtp = "smtp.gmail.com";
+            MailAddres = "ojsinfosys01@gmail.com";
+            PassWord = "ojsInfosys2020";
+            Port = 587;
+            Ssl = true;
+        }
+    }
+}
